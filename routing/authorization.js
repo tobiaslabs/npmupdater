2b0074ec4bb0e7df62e116839e65e120589626b5
Tobias Davis <accounts@davistobias.com>
2015-01-31 22:54:46 -0600

    Made everything about a bajillion times simpler than I originally had it.

--- routing/authorization.js
+++
@@ -1,9 +0,0 @@
-// This is a simplified approach, you'd probably want something more involved
-
-module.exports = function authorization(token, cb) {
-	var allowedTokens = [
-		'abc123lolbutts'
-	]
-	var isAllowed = allowedTokens.indexOf(token) >= 0
-	cb(isAllowed)
-}
--- test/test-get-recent-commit.js
+++ test/test-get-recent-commit.js
@@ -5,13 +5,16 @@ var module = {
 	name: 'fp-core',
 	user: 'sdmp',
 	repo: 'ftp-core',
-	branch: 'master',
-	npmVersion: '0.0.1'
+	version: '0.0.1'
 }
 
 test('get the most recent commit from a module', function(t) {
 	getRecentCommit(module, function(err, data) {
-		t.equal(data.name, module.name, 'module name should match')
+		t.notOk(err, 'error should not exist')
+		t.ok(data.name, 'module name should exist')
+		t.ok(data.user, 'github user should exist')
+		t.ok(data.repo, 'repo name should exist')
+		t.ok(data.version, 'version number should exist')
 		t.ok(data.sha, 'there should be a sha on the object')
 		t.end()
 	})
